52ef30e784d3bf28c08c298b2b106f79f0d62c52
Joe Reiff <jcreiff@gmail.com>
2015-06-23 23:24:11 -0400

    set root to reports/welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'reports/all_data'
-  get 'reports/welcome'
+
+  root 'reports#welcome'
   post 'reports/thank_you'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'reports#all_data'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
